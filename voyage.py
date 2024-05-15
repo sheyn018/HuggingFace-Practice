3f24eb3ff2cf88676d53c3847e33d7fff5b68f41
sheyn018 <sheane39@gmail.com>
2024-05-15 23:26:11 +0800

    first commit

---
+++ voyage.py
@@ -0,0 +1,18 @@
+import voyageai
+
+# vo = voyageai.Client()
+# This will automatically use the environment variable VOYAGE_API_KEY.
+vo = voyageai.Client(api_key="<your secret key>")
+
+texts = [
+    "The Mediterranean diet emphasizes fish, olive oil, and vegetables, believed to reduce chronic diseases.",
+    "Photosynthesis in plants converts light energy into glucose and produces essential oxygen.",
+    "20th-century innovations, from radios to smartphones, centered on electronic advancements.",
+    "Rivers provide water, irrigation, and habitat for aquatic species, vital for ecosystems.",
+    "Apple’s conference call to discuss fourth fiscal quarter results and business updates is scheduled for Thursday, November 2, 2023 at 2:00 p.m. PT / 5:00 p.m. ET.",
+    "Shakespeare's works, like 'Hamlet' and 'A Midsummer Night's Dream,' endure in literature."
+]
+
+# Embed the documents
+result = vo.embed(texts, model="voyage-large-2-instruct", input_type="document")
+print(result.embeddings)
